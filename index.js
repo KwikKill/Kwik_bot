9cc60aa340e3bc043f854f306af05628d237e83c
Gab351 <gabriel@blaisot.org>
2019-04-26 23:01:55 +0200

    bug fix

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (msg.author.bot) return undefined;
-    if (!msg.content.startsWith(prefix)) return undefined;
+    if (message.author.bot) return undefined;
+    if (!message.content.startsWith(prefix)) return undefined;
 
 
     if (message.content === prefix + 'ping') {
