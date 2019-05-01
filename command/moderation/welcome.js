6a01ce56ec541dda5ad5bbe6d9fc697a74faa637
Gab351 <gabriel@blaisot.org>
2019-05-01 17:12:28 +0200

    Bug fix

--- command/moderation/welcome.js
+++ command/moderation/welcome.js
@@ -10,37 +10,41 @@ function welcome(message) {
     return;
   }
   try {
+
+    
     let welcome = JSON.parse(fs.readFileSync("./welcome.json", "utf8"));
+
+    if (!args[0]) {
+      welcome[message.guild.id] = {
+        channel: 0
+      };
+      fs.writeFile("./welcome.json", JSON.stringify(welcome), (err) => {
+        if (err) console.log(err);
+      });
+      message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Les messages de bienvenue pour le serveur ont été désactivés.");
+    }
+    if (args[0]) {
+      if (message.guild.channels.has(arg[0])) {
+        welcome[message.guild.id] = {
+          channel: arg[0]
+        };
+        fs.writeFile("./welcome.json", JSON.stringify(welcome), (err) => {
+          if (err) console.log(err)
+        });
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `Les messages de bienvenue pour le serveur ont été activé dans le salon avec l'id : ` + arg[0] + ".");
+        //const helloworld = message.guild.channels.find(channel => channel.name === "logs");
+        //helloworld.send("**/log : /** \n  " + `Hello world!`)
+      } else {
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `salon introuvable.`)
+      }
+
+    }
+
+
   }catch(err) {
     if (err.code === 'ENOENT') {
       console.log('fichier existe pas');
     }
-  }
-
-  if (!args[0]) {
-		welcome[message.guild.id] = {
-			channel: 0
-		};
-		fs.writeFile("./welcome.json", JSON.stringify(log), (err) => {
-			if (err) console.log(err);
-		});
-		message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Les messages de bienvenue pour le serveur ont été désactivés.");
-  }
-  if (args[0]) {
-		if (message.guild.channels.has(arg[0])) {
-  		welcome[message.guild.id] = {
-  			channel: arg[0]
-  		};
-  		fs.writeFile("./welcome.json", JSON.stringify(welcome), (err) => {
-  			if (err) console.log(err)
-  		});
-  		message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `Les messages de bienvenue pour le serveur ont été activé dans le salon avec l'id : ` + arg[0] + ".");
-  		//const helloworld = message.guild.channels.find(channel => channel.name === "logs");
-  		//helloworld.send("**/log : /** \n  " + `Hello world!`)
-		} else {
-			message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `salon introuvable.`)
-		}
-
   }
 
 }
