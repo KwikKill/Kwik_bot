bcd81341f20f21253f8aae2c4a8309e62179e3ee
Gab351 <gabriel@blaisot.org>
2019-05-02 20:02:37 +0200

    Bug fix

--- command/fun/info.js
+++ command/fun/info.js
@@ -1,22 +1,48 @@
 const { Client, RichEmbed } = require('discord.js');
 function info(message) {
-  const embed = new RichEmbed()
-  .setTitle('vos stats :')
-  .setColor(0xffe402)
-  .setThumbnail(message.author.avatarURL)
-  .setDescription("vos infos :")
-  .addField(
-    "Pseudo : ",message.author.username
-  ).addField(
-    "En ligne : ", message.author.presence.status, true
-  ).addField(
-    "ID", message.author.id, true
-  ).addField(
-    "Jeux : ", message.author.presence.game, true
-  ).addField(
-    "Compte créé le : ", message.author.createdAt, true
-  );
-  message.channel.send(embed);
+
+  if(message.mentions.users.first()) {
+    let user = message.mentions.users.first();
+
+    const embed = new RichEmbed()
+    .setTitle('vos stats :')
+    .setColor(0xffe402)
+    .setThumbnail(user.avatarURL)
+    .setDescription("vos infos :")
+    .addField(
+      "Pseudo : ",user.username
+    ).addField(
+      "En ligne : ", user.presence.status, true
+    ).addField(
+      "ID", user.id, true
+    ).addField(
+      "Jeux : ", user.presence.game, true
+    ).addField(
+      "Compte créé le : ", user.createdAt, true
+    );
+    message.channel.send(embed);
+
+  }else {
+
+    const embed = new RichEmbed()
+    .setTitle('vos stats :')
+    .setColor(0xffe402)
+    .setThumbnail(message.author.avatarURL)
+    .setDescription("vos infos :")
+    .addField(
+      "Pseudo : ",message.author.username
+    ).addField(
+      "En ligne : ", message.author.presence.status, true
+    ).addField(
+      "ID", message.author.id, true
+    ).addField(
+      "Jeux : ", message.author.presence.game, true
+    ).addField(
+      "Compte créé le : ", message.author.createdAt, true
+    );
+    message.channel.send(embed);
+  }
+
 }
 
 module.exports = info;
