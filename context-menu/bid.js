42ac8989c11c6ac3445f2e9d44653f5429f239a3
KwikKill <35538496+KwikKill@users.noreply.github.com>
2021-11-25 01:29:39 +0100

    Update bid.js

--- context-menu/bid.js
+++ context-menu/bid.js
@@ -13,7 +13,7 @@ module.exports = {
             if(message.embeds[0] != undefined) {
                 if(message.embeds[0].footer.text.startsWith("Appartient à " + interaction.member.user.username)) {
                     message.startThread({name: "[A VENDRE] : " + message.embeds[0].author.name}).then(thread =>{
-                        first = thread.send(message.embeds[0].author.name + " à vendre. Pour enchérir, mettez simplement le prix dans le salon. Il n'est pas possible d'annuler une offre. L'enchère se termine au bout de 24h sans message. Vous pouvez ping <@" + interaction.member.user.id + "> si les enchères sont finis. \nEnchère minimume : denière enchère + 50")
+                        first = thread.send("Ce personnage est à vendre. Pour enchérir, mettez simplement le prix dans le salon. Il n'est pas possible d'annuler une offre. L'enchère se termine au bout de 24h sans message. Vous pouvez ping <@" + interaction.member.user.id + "> si les enchères sont finis. \nEnchère minimume : denière enchère + 50")
                         first.then(fs => {
                             fs.pin()
                         })
