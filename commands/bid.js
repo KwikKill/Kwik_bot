ca1d7fe73c070400f69bd805a9725378c845fcb9
KwikKill <35538496+KwikKill@users.noreply.github.com>
2021-12-04 00:52:15 +0100

    Update bid.js

--- commands/bid.js
+++ commands/bid.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require('path');
 const { MessageEmbed } = require('discord.js');
+const config = require('../config.json');
+const credits = require('../credits.json');
 
 module.exports = {
 	name: 'bid',
@@ -36,13 +38,32 @@ module.exports = {
             if(msg.channel.name == "mudatrade") {
                 if(msg.embeds[0] != undefined) {
                     if(msg.embeds[0].footer.text.startsWith("Appartient à " + message.author.username)) {
-                        msg.startThread({name: "[A VENDRE] : " + msg.embeds[0].author.name}).then(thread =>{
-                            first = thread.send(msg.embeds[0].author.name + " à vendre. Pour enchérir, mettez simplement le prix dans le salon. Il n'est pas possible d'annuler une offre. L'enchère se termine au bout de 24h sans message. Vous pouvez ping <@" + message.author.id + "> si les enchères sont finis. \nEnchère minimume : denière enchère + 50")
-                            first.then(fs => {
-                                fs.pin()
+                        if(config["credit"] == false) {
+                            msg.startThread({name: "[A VENDRE] : " + msg.embeds[0].author.name}).then(thread =>{
+                                first = thread.send(msg.embeds[0].author.name + " à vendre. Pour enchérir, mettez simplement le prix dans le salon. Il n'est pas possible d'annuler une offre. L'enchère se termine au bout de 24h sans message. Vous pouvez ping <@" + message.author.id + "> si les enchères sont finis. \nEnchère minimume : denière enchère + 50")
+                                first.then(fs => {
+                                    fs.pin()
+                                })
+                                message.reply("Vous avez créé une enchère sur " + msg.embeds[0].author.name + ".")
                             })
-                            message.reply("Vous avez créé une enchère sur " + msg.embeds[0].author.name + ".")
-                        })
+                        }else {
+                            if(credits[message.author.id] != undefined && credits[message.author.id] >= 1) {
+                                credits[message.author.id] = credits[message.author.id] - 1
+                                await fs.writeFile(path.join(__dirname, "..", "credits.json"), JSON.stringify(credits,null,4), (err) => {
+                                    if (err) console.log(err)
+                                });
+    
+                                msg.startThread({name: "[A VENDRE] : " + msg.embeds[0].author.name}).then(thread =>{
+                                    first = thread.send(msg.embeds[0].author.name + " à vendre. Pour enchérir, mettez simplement le prix dans le salon. Il n'est pas possible d'annuler une offre. L'enchère se termine au bout de 24h sans message. Vous pouvez ping <@" + message.author.id + "> si les enchères sont finis. \nEnchère minimume : denière enchère + 50")
+                                    first.then(fs => {
+                                        fs.pin()
+                                    })
+                                    message.reply("Vous avez créé une enchère sur " + msg.embeds[0].author.name + ".")
+                                })
+                            }else {
+                                message.reply("Vous n'avez pas assez de crédits pour créer une enchère, pour en obtenir plus, contactez <@297409548703105035>.")
+                            }
+                        }
                     }else {
                         message.reply("Vous devez être dans le salon #mudatrade pour lancer une enchère.")
                     }
