caf54bbeaedce316d1a59322626604a53266aed7
KwikKill <35538496+KwikKill@users.noreply.github.com>
2021-12-14 22:07:20 +0100

    Update emprunt.js

--- listeners/emprunt.js
+++ listeners/emprunt.js
@@ -13,15 +13,18 @@ module.exports = {
     async run(client, args) {
         if(args.reference != undefined){
             if(args.reference.messageId != undefined){
-		console.log("a")
-                msg = await args.channel.messages.fetch(args.reference.messageId)
-                if(msg.author.id == client.user.id) {
-                    if(msg.content.includes("Lisez les conditions et répondez \"lu et approuvé\" pour accepter.")) {
-                        if(msg.mentions.users.first() == args.author) {
-                            args.reply("Vous avez accepté le contrat, le contrat n'est pas annulable et serra mis en fonctionnement dès que <@297409548703105035> sera connecté.")
-                        }
-                    }
-                }
+		try {
+                	msg = await args.channel.messages.fetch(args.reference.messageId)
+			if(msg.author.id == client.user.id) {
+				if(msg.content.includes("Lisez les conditions et répondez \"lu et approuvé\" pour accepter.")) {
+					if(msg.mentions.users.first() == args.author) {
+					    args.reply("Vous avez accepté le contrat, le contrat n'est pas annulable et serra mis en fonctionnement dès que <@297409548703105035> sera connecté.")
+					}
+				}
+			}
+		}catch(err) {
+		
+		}
             }
         }
     }
