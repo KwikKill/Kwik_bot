e3950366c0cb61a917cadaeb0c52345249e86e3a
KwikKill <35538496+KwikKill@users.noreply.github.com>
2021-12-14 21:41:14 +0100

    Update bid.js

--- context-menu/bid.js
+++ context-menu/bid.js
@@ -11,11 +11,12 @@ module.exports = {
     type: "MESSAGE",
     async run(interaction, client) {
         message = await interaction.options.getMessage("message")
-	console.log(message)
         if(message.channel.name == "mudatrade") {
             if(message.embeds[0] != undefined) {
                 if(message.embeds[0].footer.text.startsWith("Appartient à " + interaction.member.user.username)) {
                     if(config["credit"] == false) {
+			console.log("c")
+			    
                         thread = await message.startThread({name: "[A VENDRE] : " + message.embeds[0].author.name})
 		        first = await thread.send("Ce personnage est à vendre. Pour enchérir, mettez simplement le prix dans le salon. Il n'est pas possible d'annuler une offre. L'enchère se termine au bout de 24h sans message. Vous pouvez ping <@" + interaction.member.user.id + "> si les enchères sont finis. \nEnchère minimum : dernière enchère + 50")
                         console.log("a")
