3e1041d09185b6f17ed9dad756dba088b1d80774
KwikKill <35538496+KwikKill@users.noreply.github.com>
2021-12-14 21:51:35 +0100

    Update bid.js

--- context-menu/bid.js
+++ context-menu/bid.js
@@ -15,7 +15,7 @@ module.exports = {
             if(message.embeds[0] != undefined) {
                 if(message.embeds[0].footer.text.startsWith("Appartient à " + interaction.member.user.username)) {
                     if(config["credit"] == false) {			    
-                        thread = await message.startThread({name: "[A VENDRE] : " + message.embeds[0].author.name})
+                        //thread = await message.startThread({name: "[A VENDRE] : " + message.embeds[0].author.name})
 			//thread.send("Ce personnage est à vendre. Pour enchérir, mettez simplement le prix dans le salon. Il n'est pas possible d'annuler une offre. L'enchère se termine au bout de 24h sans message. Vous pouvez ping <@" + interaction.member.user.id + "> si les enchères sont finis. \nEnchère minimum : dernière enchère + 50")
                         //console.log("a")
 			//console.log(first)
