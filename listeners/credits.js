16e422cea38d603750dc06854282f614ece5f234
KwikKill <35538496+KwikKill@users.noreply.github.com>
2021-12-20 19:26:31 +0100

    Create credits.js

---
+++ listeners/credits.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require('path');
+const { MessageEmbed, Interaction } = require('discord.js');
+const credits = require('../credits.json');
+
+
+module.exports = {
+    name: 'credits',
+    group: 'mudae',
+	description: "listener d'ajout de crédits",
+    type: "messageCreate",
+	place: "guild",
+    options: undefined,
+    commande_channel: true,
+    async run(client, msg) {
+        if(msg.author.id == "432610292342587392") {
+            if(msg.content.includes(" vient de donner ")) {
+                if(msg.mentions.users.get(client.user.id).id == client.user.id) {
+                    if(credits[msg.mentions.users.first()] == undefined) {
+                        credits[msg.mentions.users.first()] = 0
+                    }
+                    credits[msg.mentions.users.first()] += Math.floor(msg.content.split(" ")[4].split("**")[1] / 10)
+                    await fs.writeFile(path.join(__dirname, "..", "credits.json"), JSON.stringify(credits,null,4), (err) => {
+                        if (err) console.log(err)
+                    });
+                    msg.reply("<@" + msg.mentions.users.first().id + ">, Vous avez bien reçu " + Math.floor(msg.content.split(" ")[4].split("**")[1] / 10) + " crédits.")
+                }
+            }
+        }
+    }
+}
