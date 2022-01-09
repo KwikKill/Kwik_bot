eea5ae2b54922ec81611bf91db5a2639107a4f78
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-01-09 17:19:22 +0100

    Update weekend.js

--- listeners/weekend.js
+++ listeners/weekend.js
@@ -5,10 +5,11 @@ const { MessageEmbed, Interaction } = require('discord.js');
 module.exports = {
     name: 'weekend',
     group: 'INSA',
-	  description: "listener d'ajout de role weekend",
+    description: "listener d'ajout de role weekend",
     type: "messageReactionAdd",
-	  place: "guild",
-    async run(client, args) {
-      console.log("a")
+    place: "guild",
+    async run(client, message, user) {
+    	role = message.guild.roles.cache.get("929445487138996234")
+    	console.log(role)
     }
 }
