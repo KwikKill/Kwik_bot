bec03b24fa7498f2bffc2c19d989953535d1666f
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-01-09 17:38:21 +0100

    Update weekend.js

--- listeners/weekend.js
+++ listeners/weekend.js
@@ -9,8 +9,13 @@ module.exports = {
     type: "messageReactionAdd",
     place: "guild",
     async run(client, message, user) {
-    	role = message.message.guild.roles.cache.get("929445487138996234")
-    	member = await message.message.guild.members.fetch(user.id)
-        member.roles.add(role)
+        if(message.message.author.id == client.user.id) {
+            if(message.embeds[0].description == "Réagissez sur la réaction ✅ pour vous mettre ou enlever le rôle week end.") {
+                role = message.message.guild.roles.cache.get("929445487138996234")
+                member = await message.message.guild.members.fetch(user.id)
+                member.roles.add(role)   
+            }
+        }
+
     }
 }
