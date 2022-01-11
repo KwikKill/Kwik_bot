7eb6a5946039bf69483e4ea9178bc0da3cd24ed4
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-01-11 17:21:51 +0100

    Update roleadd.js

--- listeners/roleadd.js
+++ listeners/roleadd.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require('path');
 const { MessageEmbed, Interaction } = require('discord.js');
+const roles = require('../roles.json');
 
 module.exports = {
     name: 'roleadd',
@@ -12,10 +13,14 @@ module.exports = {
         ch = await message.message.guild.channels.fetch(message.message.channelId)
         msg = await ch.messages.fetch(message.message.id)
         
-        if(msg.embeds[0].description == "Réagissez sur la réaction ✅ pour vous mettre ou enlever le rôle week end." && message.emoji.name == "✅") {
-            role = message.message.guild.roles.cache.get("929445487138996234")
-            member = await message.message.guild.members.fetch(user.id)
-            member.roles.add(role)   
+        if(msg.embeds[0].description == "Réagissez sur un émoji pour avoir le rôle correspondant.") {
+            for(const role in roles) {
+                if(roles[role]["emoji"] == message.emoji.name) {
+                    role = message.message.guild.roles.cache.get("929445487138996234")
+                    member = await message.message.guild.members.fetch(user.id)
+                    member.roles.add(role)   
+                }
+            }
         }
     }
 }
