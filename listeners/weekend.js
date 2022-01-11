bea50f60eabca93508f138df22fda5f8fa85ae07
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-01-11 17:02:05 +0100

    Update weekend.js

--- listeners/weekend.js
+++ listeners/weekend.js
@@ -12,9 +12,7 @@ module.exports = {
         ch = await message.message.guild.channels.fetch(message.message.channelId)
         msg = await ch.messages.fetch(message.message.id)
         
-        console.log(message.emoji)
-        
-        if(msg.embeds[0].description == "Réagissez sur la réaction ✅ pour vous mettre ou enlever le rôle week end.") {
+        if(msg.embeds[0].description == "Réagissez sur la réaction ✅ pour vous mettre ou enlever le rôle week end." && message.emoji.name == "✅") {
             role = message.message.guild.roles.cache.get("929445487138996234")
             member = await message.message.guild.members.fetch(user.id)
             member.roles.add(role)   
