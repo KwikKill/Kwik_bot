b3e5b2bb10488a50be1f6b6d15d2dc270e57e85e
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-01-11 17:26:24 +0100

    Update roleremove.js

--- listeners/roleremove.js
+++ listeners/roleremove.js
@@ -15,7 +15,7 @@ module.exports = {
         if(msg.embeds[0].description == "Réagissez sur un émoji pour avoir le rôle correspondant." && user.id != client.user.id) {
             for(const role in roles) {
                 if(roles[role]["emoji"] == message.emoji.name) {
-                    role = message.message.guild.roles.cache.get("929445487138996234")
+                    role = message.message.guild.roles.cache.get(roles[role]["role"])
                     member = await message.message.guild.members.fetch(user.id)
                     member.roles.remove(role)   
                 }
