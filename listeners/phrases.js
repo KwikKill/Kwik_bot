27f8e9bfa684ef5a81d9cdfbbf480f14086d7fb1
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-01-18 18:46:50 +0100

    Create phrases.js

---
+++ listeners/phrases.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require('path');
+const { MessageEmbed, Interaction } = require('discord.js');
+
+module.exports = {
+    name: 'phrases',
+    group: 'fun',
+	  description: "listener de phrases",
+    type: "messageCreate",
+	  place: "guild",
+    options: undefined,
+    commande_channel: true,
+    async run(client, msg) {
+      guilds = ["513776796211085342"]
+      
+      if(guilds.includes(msg.channel.guild.id)) {
+       console.log("a") 
+      }
+    }
+}
