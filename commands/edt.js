85f57c00bef66b53dd36caa6aa56bbbc4e54c335
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-02-09 00:21:22 +0100

    Update edt.js

--- commands/edt.js
+++ commands/edt.js
@@ -79,7 +79,7 @@ module.exports = {
 		// Actually fill the text with a solid color
 		context.fillText("Semaine du " + monday.getUTCDate() + "/" + (monday.getUTCMonth() + 1) + "/" + monday.getUTCFullYear(), 700, 16);
 		tuesday = new Date(monday + 1000*60*60*24)
-		context.fillText("Lundi " + tuesday.getUTCDate() + "/" + (tuesday.getUTCMonth() + 1) + "/" + tuesday.getUTCFullYear(), 120, 35);
+		context.fillText("Lundi " + tuesday.getUTCDate() + "/" + (tuesday.getUTCMonth() + 1) + "/" + tuesday.getUTCFullYear(), 135, 34);
 		
 		const file = fs.createWriteStream("/opt/gab_bot/temp/file.ics");
 		var request = https.get(url_modified, function(response) {
