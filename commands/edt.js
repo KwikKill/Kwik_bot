fcd65baea7201fd13c510a667791bd8a2a3895b9
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-02-08 21:27:14 +0100

    Update edt.js

--- commands/edt.js
+++ commands/edt.js
@@ -61,7 +61,7 @@ module.exports = {
 		url_modified = url.replace("{0}", codes[interaction.options.getString("classe")]).replace("{1}", monday.getUTCFullYear() + "-" + (monday.getUTCMonth() + 1) + "-" + monday.getUTCDate()).replace("{2}", sunday.getUTCFullYear() + "-" + (sunday.getUTCMonth() + 1) + "-" + sunday.getUTCDate())
 		
 		console.log(url_modified)
-		const file = fs.createWriteStream("/home/gabriel/Gab_bot/temp/file.ics");
+		const file = fs.createWriteStream("/opt/gab_bot/temp/file.ics");
 		console.log(__filename)
 		var request = http.get(url_modified, function(response) {
 		   	 //console.log(response)
