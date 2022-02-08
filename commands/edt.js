c1045a40c709dc8c47d0480c926f2227de866dc1
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-02-08 23:25:23 +0100

    Update edt.js

--- commands/edt.js
+++ commands/edt.js
@@ -221,11 +221,11 @@ module.exports = {
 					minutes = Math.floor(duration / 60000);					
 					
 					console.log(Math.floor(47 + 297 * (start.getUTCDay() - 1)))
-					console.log(Math.floor(40 + 45.2 * (start.getHours() + 1 + (start.getMinutes()/60))))
+					console.log(Math.floor((start.getHours() + 1 + (start.getMinutes()/60))))
 					console.log(Math.floor(295*(hours + (minutes/60))))
 					
 					context.beginPath();
-					context.rect(47, Math.floor(40 + 45.2 * (start.getHours() + 1 + (start.getMinutes()/60))), 295, 43);
+					context.rect(Math.floor(47 + 297 * (start.getUTCDay() - 1)), Math.floor(40 + 45.2 * (start.getHours() + 1 + (start.getMinutes()/60))), 295, 43);
 					context.fillStyle = 'yellow';
 					context.fill();
 					context.lineWidth = 2;
