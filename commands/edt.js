be420524a5656f5f1621321d5db758992f84d6f4
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-02-09 13:44:34 +0100

    Update edt.js

--- commands/edt.js
+++ commands/edt.js
@@ -166,7 +166,7 @@ module.exports = {
 							context.textAlign = "center"
 							context.font = '12px sans-serif';
 							context.fillStyle = '#000000';
-							context.fillText(summary, Math.floor(47 + 297 * (start.getUTCDay() - 1) + width*h) + width, Math.floor(40 + 45.2 * (start.getHours() - 7 + (start.getMinutes()/60))) + 20, width);
+							context.fillText(summary, (Math.floor(47 + 297 * (start.getUTCDay() - 1) + width*h) + Math.floor(47 + 297 * (start.getUTCDay() - 1) + width*h) + width)/2, Math.floor(40 + 45.2 * (start.getHours() - 7 + (start.getMinutes()/60))) + 20, width);
 						}
 					}
 				}
