4819fe2baa9e1d64cc20d9c14155caa80007e6fc
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-02-25 23:18:42 +0100

    Update phrases.js

--- listeners/phrases.js
+++ listeners/phrases.js
@@ -74,7 +74,7 @@ module.exports = {
 	}
 	
 	for(x in arr) {
-	  if(arr[x].startsWith("ukraine") || arr[x].startsWith("l'ukraine")) {
+	  if(arr[x].startsWith("ukraine") || arr[x].startsWith("lukraine")) {
 		msg.reply("L'Ukraine ? tu veut dire cette province de la Russie ?")
 		return;
 	  }
