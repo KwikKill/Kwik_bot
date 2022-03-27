fd777d13810325c4fdcc68e37db488dcff9e1f00
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-03-27 18:40:55 +0200

    Update edt.js

--- commands/edt.js
+++ commands/edt.js
@@ -121,7 +121,7 @@ module.exports = {
 					if(di[start.getDay()][start.getHours() + "-" + start.getUTCMinutes()] == undefined) {
 						di[start.getDay()][start.getHours() + "-" + start.getUTCMinutes()] = [{"summary": event.summary, "start": start, "end": end, "description": event.description, "location": event.location}]
 					}else {
-						di[start.getDay()][(start.getHours() + "-" + start.getUTCMinutes()].push({"summary": event.summary, "start": start, "end": end, "description": event.description, "location": event.location})
+						di[start.getDay()][start.getHours() + "-" + start.getUTCMinutes()].push({"summary": event.summary, "start": start, "end": end, "description": event.description, "location": event.location})
 					}
 				}
 				
