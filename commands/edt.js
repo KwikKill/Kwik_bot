92abffc8dad549658dd10ba6cd470b85b2d625b9
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-03-27 18:40:31 +0200

    Update edt.js

--- commands/edt.js
+++ commands/edt.js
@@ -114,13 +114,14 @@ module.exports = {
 				di = {1:{}, 2:{}, 3:{}, 4:{}, 5:{}}
 				for (const event of Object.values(events)) {
 					start = new Date(event.start.toISOString())
-					//start.setHours(start)
+					start.setHours(start.getHours() + 1)
 					end = new Date(event.end.toISOString())
+					end.setHours(end.getHours() + 1)
 					
 					if(di[start.getDay()][start.getHours() + "-" + start.getUTCMinutes()] == undefined) {
 						di[start.getDay()][start.getHours() + "-" + start.getUTCMinutes()] = [{"summary": event.summary, "start": start, "end": end, "description": event.description, "location": event.location}]
 					}else {
-						di[start.getDay()][start.getHours() + "-" + start.getUTCMinutes()].push({"summary": event.summary, "start": start, "end": end, "description": event.description, "location": event.location})
+						di[start.getDay()][(start.getHours() + "-" + start.getUTCMinutes()].push({"summary": event.summary, "start": start, "end": end, "description": event.description, "location": event.location})
 					}
 				}
 				
