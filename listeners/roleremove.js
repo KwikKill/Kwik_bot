abc6bddd6931869aaaf1a9b58b36d131a48aff8d
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-04-04 00:29:20 +0200

    Update roleremove.js

--- listeners/roleremove.js
+++ listeners/roleremove.js
@@ -15,13 +15,14 @@ module.exports = {
         }
         ch = await message.message.guild.channels.fetch(message.message.channelId)
         msg = await ch.messages.fetch(message.message.id)
-        
-        if(msg.embeds[0].description == "Réagissez sur un émoji pour avoir le rôle correspondant." && user.id != client.user.id) {
-            for(const role in roles) {
-                if(roles[role]["emoji"] == message.emoji.name) {
-                    roletoadd = message.message.guild.roles.cache.get(roles[role]["role"])
-                    member = await message.message.guild.members.fetch(user.id)
-                    member.roles.remove(roletoadd)   
+        if(msg.embeds[0] != undefined) {
+            if(msg.embeds[0].description == "Réagissez sur un émoji pour avoir le rôle correspondant." && user.id != client.user.id) {
+                for(const role in roles) {
+                    if(roles[role]["emoji"] == message.emoji.name) {
+                        roletoadd = message.message.guild.roles.cache.get(roles[role]["role"])
+                        member = await message.message.guild.members.fetch(user.id)
+                        member.roles.remove(roletoadd)   
+                    }
                 }
             }
         }
