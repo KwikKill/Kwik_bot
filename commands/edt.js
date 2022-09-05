51bfbb24e715893c75b85ee6efabbb4556da020b
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-09-05 21:55:20 +0200

    Update edt.js

--- commands/edt.js
+++ commands/edt.js
@@ -180,7 +180,7 @@ module.exports = {
 						}
 
 						test.push(x)
-						console.log(test.length, codes[interaction.options.getString("classe").toLowerCase()].keys().length)
+						console.log(test.length, codes[interaction.options.getString("classe").toLowerCase()])
 						if(test.length == codes[interaction.options.getString("classe").toLowerCase()].length) {
 							for(const j in di) {
 								for(const i in di[j]) {
