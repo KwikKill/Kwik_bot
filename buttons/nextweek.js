ba11755e420c4c5316209f3df03052f20a29bd83
KwikKill <gabriel@blaisot.org>
2022-09-14 17:17:56 +0200

    test(edt): test

--- buttons/nextweek.js
+++ buttons/nextweek.js
@@ -9,7 +9,8 @@ module.exports = {
 	permission: "all",
 	serverid: ["513776796211085342", "890915473363980308"],
     async run(interaction, client) {
+        interaction.reply({content: "a...", ephemeral: true});
         console.log(interaction.message.embeds[0])
-        interaction.reply({content: "Reset de la semaine en cours...", ephemeral: true});
+        
   }
 }
