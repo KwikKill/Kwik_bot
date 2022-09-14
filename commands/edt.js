1451e18b4c5caa3e68d742c1b9d2de1c3657e354
KwikKill <gabriel@blaisot.org>
2022-09-14 17:27:47 +0200

    test(edt): test

--- commands/edt.js
+++ commands/edt.js
@@ -283,7 +283,7 @@ module.exports = {
 								}
 							}
 							const row = new MessageActionRow()
-							/*.addComponents(
+							.addComponents(
 								new MessageButton()
 								.setCustomId('lastweek')
 								.setLabel('◀️')
@@ -294,9 +294,9 @@ module.exports = {
 								.setCustomId('nextweek')
 								.setLabel('▶️')
 								.setStyle('PRIMARY'),
-							);*/
+							);
 							
-							interaction.editReply({embeds: [embed1], files: [attachment], components: [row]});
+							interaction.editReply({embeds: [embed1], files: [attachment]});//, components: [row]});
 							//interaction.editReply({content: "Emploi du temp de la classe : " + interaction.options.getString("classe").toLowerCase(), files: [canvas.toBuffer()]})
 
 						}
@@ -471,7 +471,7 @@ module.exports = {
 				.setImage(`attachment://edt.png`);
 
 				const row = new MessageActionRow()
-				/*.addComponents(
+				.addComponents(
 					new MessageButton()
 					.setCustomId('lastweek')
 					.setLabel('◀️')
@@ -494,9 +494,9 @@ module.exports = {
 					.setCustomId('nextclass')
 					.setLabel('⏭️')
 					.setStyle('PRIMARY'),
-				);*/
+				);
 				
-				interaction.editReply({embeds: [embed1], files: [attachment], components: [row]});
+				interaction.editReply({embeds: [embed1], files: [attachment]});//, components: [row]});
 				//interaction.editReply({content: "Emploi du temp de la classe : " + interaction.options.getString("classe").toLowerCase(), files: [canvas.toBuffer()]})
 			});
 		}).on('error', function(err) {
