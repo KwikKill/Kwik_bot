a54c0a44465db311a476212e89f0de2d24e587eb
KwikKill <gabriel@blaisot.org>
2022-09-15 22:24:35 +0200

    test(edt): test

--- buttons/nextclass.js
+++ buttons/nextclass.js
@@ -9,16 +9,16 @@ module.exports = {
 	permission: "all",
 	serverid: ["513776796211085342", "890915473363980308"],
     async run(interaction, client) {
-        test = JSON.parse(JSON.stringify(client.commands.get("edt").codes))
-        delete test["raph"]
-        console.log(Object.keys(test))
-        // 
-        return;
         classs = interaction.message.embeds[0].title.replace("Emploi du temp de la classe : ", '')
         date1 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[0])
         date2 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[2])
-        date1.setDate(date1.getDate() - 7)
-        date2.setDate(date2.getDate() - 7)
+
+        test = JSON.parse(JSON.stringify(client.commands.get("edt").codes))
+        delete test["raph"]
+        keys = Object.keys(test)
+        console.log(keys.indexOf(classs))
+        // 
+        return;
         // edit
         if(classs == "raph") {
             client.commands.get("edt").create_di_raph(client, date1, date2, interaction, "raph");
