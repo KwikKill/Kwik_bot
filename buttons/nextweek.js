5bf18a46fa2e405fb458f73fbea7eb326e7680fe
KwikKill <gabriel@blaisot.org>
2022-09-15 21:48:52 +0200

    test(edt): test

--- buttons/nextweek.js
+++ buttons/nextweek.js
@@ -15,7 +15,7 @@ module.exports = {
         a2.setDate(a2.getDate() + 7)
         a3 = new Date(a2)
 		a3 = new Date(a3.setDate(a3.getDate() + 4));
-        client.commands.get("edt").create_di_raph(a2, a3, interaction, "raph");
+        client.commands.get("edt").create_di_raph(client, a2, a3, interaction, "raph");
         console.log(interaction.message.embeds[0].title.replace("Emploi du temp de la classe : ", ''));
         console.log(interaction.message.embeds[0].description.replace("Semaine du ", ''));
         
