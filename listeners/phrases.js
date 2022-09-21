c375b12986aba0b4864f265abde6fb9d3457343e
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-09-21 18:40:59 +0200

    Update phrases.js

--- listeners/phrases.js
+++ listeners/phrases.js
@@ -18,6 +18,7 @@ module.exports = {
 	    
       if(msg.channel.id != "940543958394732555") {
 	      for(const x in hiragana) {
+		      console.log(x)
 		if(msg.content.includes(x)) {
 		   	console.log("a")
 		   }
