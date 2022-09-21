4c0eb76d659f1ceb84335b64a583bf9aeef857bd
KwikKill <35538496+KwikKill@users.noreply.github.com>
2022-09-21 20:05:29 +0200

    Update phrases.js

--- listeners/phrases.js
+++ listeners/phrases.js
@@ -4,6 +4,9 @@ const { MessageEmbed, Interaction } = require('discord.js');
 
 hiragana = ["あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を","ん","が","ぎ","ぐ","げ","ご","ざ",
 	    "じ","ず","ぜ","ぞ","だ","じ","づ","で","ど","ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ"]
+katakana = ["ア","イ","ウ","エ","オ","カ","キ","ク","ケ","コ","サ","シ","ス","セ","ソ","タ","チ","ツ","テ","ト","ナ","ニ","ヌ","ネ","ノ",
+	    "ハ","ヒ","フ","ヘ","ホ","マ","ミ","ム","メ","モ","ヤ","ユ","ヨ","ラ","リ","ル","レ","ロ","ワ","ヲ","ン","ガ","ギ","グ","ゲ","ゴ",
+	    "ザ","ジ","ズ","ゼ","ゾ","ダ","ヂ","ヅ","デ","ド","バ","ビ","ブ","ベ","ボ","パ","ピ","プ","ペ","ポ"]
 
 module.exports = {
     name: 'phrases',
@@ -16,9 +19,22 @@ module.exports = {
     async run(client, msg) {
       guilds = ["513776796211085342", "890915473363980308", "480142959501901845"]
 	    
-      if(msg.channel.id != "940543958394732555") {
+      if(msg.channel.id != "940543958394732555" && msg.guild.id == "890915473363980308") {
 	      for(const x in hiragana) {
 		if(msg.content.includes(hiragana[x])) {
+			client.channels.fetch("1004443609355002027").then(general => {
+				msg.author.send("pas de japoniaiserie dans les salons autre que <#940543958394732555>");
+				msg.guild.invites.create(general).then(invite => {
+					msg.author.send(invite.url)
+				})
+				general.send("<@" + msg.author.id + "> a été kick pour utilisation de japoniaiserie non autorisé.")
+				msg.member.kick();
+		   		return;
+			});
+		   }
+	      }
+	      for(const x in katakana) {
+		if(msg.content.includes(katakana[x])) {
 			client.channels.fetch("1004443609355002027").then(general => {
 				msg.author.send("pas de japoniaiserie dans les salons autre que <#940543958394732555>");
 				msg.guild.invites.create(general).then(invite => {
