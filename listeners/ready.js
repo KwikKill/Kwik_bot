f6f7fc823975ababb4d0d6afdf8cc43a994f2db4
KwikKill <gabriel@blaisot.org>
2022-10-27 02:04:58 +0200

    add require('pg')

--- listeners/ready.js
+++ listeners/ready.js
@@ -1,6 +1,7 @@
 const config = require('../config.json');
 const fs = require("fs");
 const path = require('path');
+const pg = require('pg')
 const packageJSON = require("../package.json");
 const { MessageEmbed, Interaction } = require('discord.js');
 
