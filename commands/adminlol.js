7d311b9562264b5c4d5a49de3466cf0882f01347
KwikKill <gabriel@blaisot.org>
2022-10-29 12:59:15 +0200

    fix command name

--- commands/adminlol.js
+++ commands/adminlol.js
@@ -1,7 +1,7 @@
 //const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'lol',
+    name: 'adminlol',
     group: 'lol',
     description: "Commande lol",
     permission: "none",
