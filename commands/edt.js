e268015be6fcd3d8d3696abcd531d79f7d9fb971
KwikKill <gabriel@blaisot.org>
2022-10-29 12:08:31 +0200

    fix bug

--- commands/edt.js
+++ commands/edt.js
@@ -226,7 +226,7 @@ function create_di(events) {
         const end = new Date(event.end.toISOString());
         end.setHours(end.getUTCHours() + 1);
 
-        if (start.getDay() !== 0 && start.getDay() !== 6) {
+        if (start.getDay() !== 0 || start.getDay() !== 6) {
             if (di[start.getDay()][start.getUTCHours() + "-" + start.getUTCMinutes()] === undefined) {
                 di[start.getDay()][start.getUTCHours() + "-" + start.getUTCMinutes()] = [{ "summary": event.summary, "start": start, "end": end, "description": event.description, "location": event.location }];
             } else {
