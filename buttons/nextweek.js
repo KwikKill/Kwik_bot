ec044c6b1c08737b8de4fbb7b9f19af6cde99daf
KwikKill <gabriel@blaisot.org>
2022-10-29 12:26:41 +0200

    test debug

--- buttons/nextweek.js
+++ buttons/nextweek.js
@@ -8,11 +8,11 @@ module.exports = {
         const classs = interaction.message.embeds[0].title.replace("Emploi du temp de la classe : ", '');
         const date1 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[0]);
         const date2 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[2]);
-        console.log(date1.getDate(), date1.getDate() + 7);
+        console.log(date1.getDate(), date1.getDate() + 7.1);
         console.log(date1);
-        date1.setDate(date1.getDate() + 7);
+        date1.setDate(date1.getDate() + 7.1);
         console.log(date1);
-        date2.setDate(date2.getDate() + 7);
+        date2.setDate(date2.getDate() + 7.1);
         // edit
         if (classs === "raph") {
             client.commands.get("edt").create_di_raph(client, date1, date2, interaction, "raph");
