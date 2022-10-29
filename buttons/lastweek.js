703b9c46e99ecd0dcf0e8c78b1948ffaa0226b96
KwikKill <gabriel@blaisot.org>
2022-10-29 12:32:47 +0200

    test debug

--- buttons/lastweek.js
+++ buttons/lastweek.js
@@ -13,7 +13,7 @@ module.exports = {
         date1.setDate(date1.getDate() - 7);
         date1.setTime(date1.getTime() + 1 * 60 * 60 * 1000);
         date2.setDate(date2.getDate() - 7);
-        date2.setTime(date1.getTime() + 1 * 60 * 60 * 1000);
+        date2.setTime(date2.getTime() + 1 * 60 * 60 * 1000);
         // edit
         if (classs === "raph") {
             client.commands.get("edt").create_di_raph(client, date1, date2, interaction, "raph");
