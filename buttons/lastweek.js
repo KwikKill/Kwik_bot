03735a3a2a89920e14bffc882797af91a2265195
KwikKill <gabriel@blaisot.org>
2022-10-29 12:31:45 +0200

    test debug

--- buttons/lastweek.js
+++ buttons/lastweek.js
@@ -11,7 +11,9 @@ module.exports = {
         const date1 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[0]);
         const date2 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[2]);
         date1.setDate(date1.getDate() - 7);
+        date1.setTime(date1.getTime() + 1 * 60 * 60 * 1000);
         date2.setDate(date2.getDate() - 7);
+        date2.setTime(date1.getTime() + 1 * 60 * 60 * 1000);
         // edit
         if (classs === "raph") {
             client.commands.get("edt").create_di_raph(client, date1, date2, interaction, "raph");
--- buttons/nextweek.js
+++ buttons/nextweek.js
@@ -8,12 +8,10 @@ module.exports = {
         const classs = interaction.message.embeds[0].title.replace("Emploi du temp de la classe : ", '');
         const date1 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[0]);
         const date2 = new Date(interaction.message.embeds[0].description.replace("Semaine du ", '').split(" ")[2]);
-        console.log(date1.getDate(), date1.getDate() + 7);
-        console.log(date1);
         date1.setDate(date1.getDate() + 7);
         date1.setTime(date1.getTime() + 1 * 60 * 60 * 1000);
-        console.log(date1);
         date2.setDate(date2.getDate() + 7);
+        date1.setTime(date2.getTime() + 1 * 60 * 60 * 1000);
         // edit
         if (classs === "raph") {
             client.commands.get("edt").create_di_raph(client, date1, date2, interaction, "raph");
