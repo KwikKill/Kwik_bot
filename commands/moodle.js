2c7b48288301906bd31a6350dd8a8d6039676f0f
KwikKill <gabriel@blaisot.org>
2022-11-13 14:40:40 +0100

    fix interaction already responded

--- commands/moodle.js
+++ commands/moodle.js
@@ -29,23 +29,22 @@ module.exports = {
                     .setTimestamp();
                 return interaction.reply({ embeds: [embed] });
             }
-        });
-
-        https.get('https://moodleng.insa-rennes.fr/', (res) => {
-            if (res.statusCode === 200) {
+            https.get('https://moodleng.insa-rennes.fr/', (res) => {
+                if (res.statusCode === 200) {
+                    const embed = new MessageEmbed()
+                        .setColor("0xffe402")
+                        .setTitle("Moodle")
+                        .setDescription("Moodle marche")
+                        .setTimestamp();
+                    return interaction.reply({ embeds: [embed] });
+                }
                 const embed = new MessageEmbed()
                     .setColor("0xffe402")
                     .setTitle("Moodle")
-                    .setDescription("Moodle marche")
+                    .setDescription("Moodle ne marche pas <:503:1034543332283650169>")
                     .setTimestamp();
                 return interaction.reply({ embeds: [embed] });
-            }
-            const embed = new MessageEmbed()
-                .setColor("0xffe402")
-                .setTitle("Moodle")
-                .setDescription("Moodle ne marche pas <:503:1034543332283650169>")
-                .setTimestamp();
-            return interaction.reply({ embeds: [embed] });
+            });
         });
     }
 };
