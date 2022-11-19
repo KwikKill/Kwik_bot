dd670fac0b83337bd20b16336cc7d03a82937d6a
KwikKill <gabriel@blaisot.org>
2022-11-19 14:05:35 +0100

    fix

--- util/express.js
+++ util/express.js
@@ -60,7 +60,7 @@ function register(client) {
 
     app.get("/lol/summoner", function (req, res) {
         if (req.query.discordid) {
-            client.pg.query("SELECT * FROM summoners WHERE discordid = '$1'", [req.query.discordid], (err, result) => {
+            client.pg.query('SELECT * FROM summoners WHERE discordid = \'$1\'', [req.query.discordid], (err, result) => {
                 if (err) { throw err; }
                 if (result.rows.length > 0) {
                     return res.send(result.rows);
@@ -83,7 +83,7 @@ function register(client) {
     app.get("/lol/matchs", function (req, res) {
         console.log(req.query);
         if (req.query.discordid) {
-            client.pg.query("SELECT * FROM matchs, summoners WHERE player = summoners.puuid AND discordid = '$1'", [req.query.discordid], (err, result) => {
+            client.pg.query('SELECT * FROM matchs, summoners WHERE player = summoners.puuid AND discordid = \'$1\'', [req.query.discordid], (err, result) => {
                 if (err) { throw err; }
                 if (result.rows.length > 0) {
                     return res.send(result.rows);
@@ -92,7 +92,7 @@ function register(client) {
             });
         } else {
             if (req.query.puuid) {
-                client.pg.query("SELECT * FROM matchs WHERE player = '$1'", [req.query.puuid], (err, result) => {
+                client.pg.query('SELECT * FROM matchs WHERE player = \'$1\'', [req.query.puuid], (err, result) => {
                     if (err) { throw err; }
                     if (result.rows.length > 0) {
                         return res.send(result.rows);
@@ -121,6 +121,6 @@ function register(client) {
     });
 
     app.listen(8080, () => {
-        console.log("Serveur à l'écoute");
+        console.log("Express server started");
     });
 }
