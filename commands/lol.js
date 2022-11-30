e263fa3ed8b37f36f754de8c1b460b10505c1945
KwikKill <gabriel@blaisot.org>
2022-11-30 18:07:16 +0100

    test

--- commands/lol.js
+++ commands/lol.js
@@ -1160,6 +1160,8 @@ module.exports = {
                 query += " GROUP BY matchs.matchup ORDER BY count1 DESC;";
                 console.log(query);
                 const response = await client.pg.query(query);
+                console.log(response.rows);
+                console.log(response);
                 if (response.rows.length === 0) {
                     return await interaction.editReply("You don't have any matchs in the database or the filters are too restrictings.");
                 }
