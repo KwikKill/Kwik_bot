0c66beca45829f8ba6228f173e7e60dee3322626
KwikKill <gabriel@blaisot.org>
2022-12-08 16:34:20 +0100

    test

--- Site/js/custom.js
+++ Site/js/custom.js
@@ -21,17 +21,3 @@ function httpGetAsync(theUrl, callback) {
   xmlHttp.open("GET", theUrl, true); // true for asynchronous 
   xmlHttp.send(null);
 }
-
-$(document).ready(function () {
-  $(".moreBox").slice(0, 3).show();
-  if ($(".blogBox:hidden").length != 0) {
-    $("#loadMore").show();
-  }
-  $("#loadMore").on('click', function (e) {
-    e.preventDefault();
-    $(".moreBox:hidden").slice(0, 6).slideDown();
-    if ($(".moreBox:hidden").length == 0) {
-      $("#loadMore").fadeOut('slow');
-    }
-  });
-});
