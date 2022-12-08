dd629b89276b32dd2c790665b8c8e151553a6680
KwikKill <gabriel@blaisot.org>
2022-12-08 18:49:22 +0100

    fix

--- util/express.js
+++ util/express.js
@@ -184,7 +184,7 @@ function register(client) {
                             return res.sendStatus(403);
                         }
                         if (result.rows.length > 0) {
-                            client.pg.query('SELECT puuid, champion FROM matchs, summoners WHERE matchs.player = summoners.puuid AND discordid = $1 ORDER BY timestamp DESC LIMIT 10;', [data.id], (err2, result2) => {
+                            client.pg.query('SELECT matchs.puuid FROM matchs, summoners WHERE matchs.player = summoners.puuid AND discordid = $1 ORDER BY timestamp DESC LIMIT 10;', [data.id], (err2, result2) => {
                                 return res.render('../Site/lol/matchs', { username: data.username, discriminator: data.discriminator, avatar: data.avatar, games: result2.rows });
                             });
                         } else {
