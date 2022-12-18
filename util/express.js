1285757fec446d65405ca9828897adac9bf1fbbf
KwikKill <gabriel@blaisot.org>
2022-12-19 00:44:24 +0100

    fix

--- util/express.js
+++ util/express.js
@@ -193,11 +193,9 @@ function register(client) {
                             return res.sendStatus(403);
                         }
                         if (result.rows.length > 0) {
-                            client.pg.query('SELECT matchs.puuid FROM matchs, summoners WHERE matchs.player = summoners.puuid AND discordid = $1 ORDER BY timestamp DESC LIMIT 10;', [data.id], (err2, result2) => {
+                            client.pg.query('SELECT matchs.puuid, matchs.champion FROM matchs, summoners WHERE matchs.player = summoners.puuid AND discordid = $1 ORDER BY timestamp DESC LIMIT 10;', [data.id], (err2, result2) => {
                                 lol_api.getCurrentPatch("EUW1", client).then(version => {
-                                    console.log(version);
                                     lol_api.getChampsId("EUW1", client).then(dict => {
-                                        console.log(dict);
                                         return res.render('../Site/lol/matchs', { username: data.username, discriminator: data.discriminator, avatar: data.avatar, games: result2.rows, version: version, dict: dict });
                                     });
                                 });
