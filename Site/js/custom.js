e1f409bb223a4e3aa1d5cebb47b990422714788a
KwikKill <gabriel@blaisot.org>
2022-12-29 21:03:08 +0100

    test

--- Site/js/custom.js
+++ Site/js/custom.js
@@ -24,9 +24,17 @@ function httpGetAsync(theUrl, callback) {
 
 loadMore = function () {
   var table = document.getElementById("matchsList");
-  var row = table.insertRow(table.rows.length - 1);
-  var cell = row.insertCell(0);
-  cell.innerHTML = "Test2";
+  var rowCount = table.rows.length;
+  table.deleteRow(rowCount - 1);
+
+  httpGetAsync("http://albert.blaisot.org:8080/lol/matchs", function (response) {
+    var table = document.getElementById("matchsListbody");
+    table.innerHTML += response;
+    table.innerHTML += "<tr class=\"see_more_ajax_button_row\"><td colspan=\"10\" class=\"text-center\"><button type=\"button\" class=\"see_more\" onclick=\"loadMore()\">See more</button></td></tr>";
+  });
+  //var row = table.insertRow(table.rows.length - 1);
+  //var cell = row.insertCell(0);
+  //cell.innerHTML = "Test2";
 }
 
 initial_load = function () {
