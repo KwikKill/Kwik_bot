c31c4ba553106718f5a6d1d53124667afe4254cc
KwikKill <gabriel@blaisot.org>
2022-12-30 02:18:56 +0100

    test

--- util/express.js
+++ util/express.js
@@ -126,6 +126,7 @@ function register(client) {
                     if (al) {
                         return res.render("../Site/lol/message", { text: "This account is already registered" });
                     }
+                    console.log(req.body.username, req.body.discordid);
                     client.commands.get("lol").add_summoner_manual(client, req.body.username, req.body.discordid);
                     delay(1800).then(() => {
                         return res.redirect("/lol/queue");
