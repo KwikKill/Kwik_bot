2d87f3853965a4a700bda3c33bfeed85b7ef950b
KwikKill <gabriel@blaisot.org>
2023-03-17 00:25:11 +0100

    feat(markov): add profiles, options and more

--- commands/markov.js
+++ commands/markov.js
@@ -1,3 +1,6 @@
+const Markov = require('markov-strings').default;
+const fs = require('fs');
+
 module.exports = {
     name: 'markov',
     group: 'fun',
@@ -6,41 +9,103 @@ module.exports = {
     serverid: ["513776796211085342", "480142959501901845", "890915473363980308", "962329252550807592"],
     hidden: false,
     place: "guild",
-    options: undefined,
+    options: [
+        {
+            name: 'score',
+            description: 'score minimum du message',
+            type: 'STRING',
+            required: false,
+        },
+        {
+            name: 'profil',
+            description: 'profil de markov',
+            type: 'USER',
+            required: false,
+        }
+    ],
     async run(message, client, interaction = undefined) {
         if (interaction !== undefined) {
             await interaction.deferReply();
-            const options = {
-                maxTries: 2000,
-                prng: Math.random,
+            if (interaction.options.getUser('profil') === null || interaction.options.getUser('profil') === undefined) {
+                let score = 10;
+                if (interaction.options.getString('score') !== null) {
+                    score = interaction.options.getString('score');
+                }
+                const options = {
+                    maxTries: 2000,
+                    prng: Math.random,
 
-                filter: (result) => {
-                    if (result.string.split(' ').length < 5) {
-                        return false;
-                    }
-                    if (result.refs.length === 1) {
-                        return false;
-                    }
-                    if (result.score <= 10) {
-                        return false;
-                    }
-                    if (result.string[result.string.length - 1] !== "." && result.string[result.string.length - 1] !== "!" && result.string[result.string.length - 1] !== "?") {
-                        return false;
-                    }
-                    for (const ref of result.refs) {
-                        if (result.string === ref.string) {
+                    filter: (result) => {
+                        if (result.string.split(' ').length < 5) {
                             return false;
                         }
+                        if (result.refs.length === 1) {
+                            return false;
+                        }
+                        if (result.score <= score) {
+                            return false;
+                        }
+                        if (result.string[result.string.length - 1] !== "." && result.string[result.string.length - 1] !== "!" && result.string[result.string.length - 1] !== "?") {
+                            return false;
+                        }
+                        for (const ref of result.refs) {
+                            if (result.string === ref.string) {
+                                return false;
+                            }
+                        }
+                        return true;
                     }
-                    return true;
+                };
+                try {
+                    const content = client.markov.generate(options);
+                    interaction.editReply("Markov : " + content.string);
+                } catch (e) {
+                    interaction.editReply("Markov : ratio (j'ai la flemme)");
                 }
-            };
-            try {
-                const content = client.markov.generate(options);
-                interaction.editReply("Markov : " + content.string);
-            } catch (e) {
-                interaction.editReply("Markov : Et si je veux pas générer de message ?");
-                console.log(e);
+            } else {
+                const userid = interaction.options.getUser('profil').id;
+                let score = 4;
+                if (interaction.options.getString('score') !== null) {
+                    score = interaction.options.getString('score');
+                }
+                const options = {
+                    maxTries: 2000,
+                    prng: Math.random,
+
+                    filter: (result) => {
+                        if (result.string.split(' ').length < 5) {
+                            return false;
+                        }
+                        if (result.refs.length === 1) {
+                            return false;
+                        }
+                        if (result.score <= score) {
+                            return false;
+                        }
+                        if (result.string[result.string.length - 1] !== "." && result.string[result.string.length - 1] !== "!" && result.string[result.string.length - 1] !== "?") {
+                            return false;
+                        }
+                        for (const ref of result.refs) {
+                            if (result.string === ref.string) {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                };
+                try {
+                    const markov = new Markov({ stateSize: 3 });
+                    markov.import(JSON.parse(fs.readFileSync('markov/' + userid + '.json', 'utf8')));
+                    try {
+                        const content = markov.generate(options);
+                        interaction.editReply("Markov : " + content.string);
+                    } catch (e) {
+                        interaction.editReply("Markov : ratio (j'ai la flemme)");
+                    }
+                } catch (e) {
+                    interaction.editReply("Ce profile n'existe pas");
+                    return;
+                }
             }
         }
     }
