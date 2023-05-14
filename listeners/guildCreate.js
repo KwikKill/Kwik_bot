51a0e25e4a363af9d31bbde6959bac7cff39b54f
KwikKill <gabriel@blaisot.org>
2023-05-14 10:25:50 +0200

    fix(guildCreate): remove message sending

--- listeners/guildCreate.js
+++ listeners/guildCreate.js
@@ -6,22 +6,6 @@ module.exports = {
     place: "guild",
     options: undefined,
     async run(client, guild) {
-        const first_channel = guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.permissionsFor(guild.members.me).has("SEND_MESSAGES"));
-        let message;
-        if (first_channel) {
-            try {
-                message = await first_channel.send({ content: "Deploying commands,..." });
-            } catch (error) {
-                console.log(error);
-            }
-        }
         await client.commands.get("deploy").deploy(client, guild);
-        if (message) {
-            try {
-                await message.edit("Deployed !");
-            } catch (error) {
-                console.log(error);
-            }
-        }
     }
 };
