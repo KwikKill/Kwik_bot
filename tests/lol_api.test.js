fd2f4e599046baa988ecc6e0ad8169131796a3f6
KwikKill <gabriel@blaisot.org>
2023-05-27 14:18:22 +0200

    feat(tests): add unit tests for challenges API call

--- tests/lol_api.test.js
+++ tests/lol_api.test.js
@@ -2,29 +2,30 @@ const { LolApi } = require('../util/lol_api');
 const { describe, it, expect, beforeEach } = require('@jest/globals');
 
 const api_key = process.env.RIOT_API_KEY;
+const client_preset = {
+    'lol': {
+        "api_limit": false,
+        "route": {
+            "EUW1": "EUROPE",
+            "NA1": "AMERICAS",
+            "KR": "ASIA",
+            "EUN1": "EUROPE",
+            "BR1": "AMERICAS",
+            "JP1": "ASIA",
+            "LA1": "AMERICAS",
+            "LA2": "AMERICAS",
+            "OC1": "SEA",
+            "TR1": "EUROPE",
+            "RU": "EUROPE"
+        }
+    }
+};
 
 describe('getChampsId', () => {
     let client;
 
     beforeEach(() => {
-        client = {
-            'lol': {
-                "api_limit": false,
-                "route": {
-                    "EUW1": "EUROPE",
-                    "NA1": "AMERICAS",
-                    "KR": "ASIA",
-                    "EUN1": "EUROPE",
-                    "BR1": "AMERICAS",
-                    "JP1": "ASIA",
-                    "LA1": "AMERICAS",
-                    "LA2": "AMERICAS",
-                    "OC1": "SEA",
-                    "TR1": "EUROPE",
-                    "RU": "EUROPE"
-                }
-            }
-        }; // mock client object
+        client = client_preset; // mock client object
     });
 
     it('should return an object with champion names as keys and IDs as values', async () => {
@@ -55,24 +56,7 @@ describe('summonersByName', () => {
     let client;
 
     beforeEach(() => {
-        client = {
-            'lol': {
-                "api_limit": false,
-                "route": {
-                    "EUW1": "EUROPE",
-                    "NA1": "AMERICAS",
-                    "KR": "ASIA",
-                    "EUN1": "EUROPE",
-                    "BR1": "AMERICAS",
-                    "JP1": "ASIA",
-                    "LA1": "AMERICAS",
-                    "LA2": "AMERICAS",
-                    "OC1": "SEA",
-                    "TR1": "EUROPE",
-                    "RU": "EUROPE"
-                }
-            }
-        }; // mock client object
+        client = client_preset; // mock client object
     });
 
     it('should return a summoner object when given valid input', async () => {
@@ -104,24 +88,7 @@ describe('summonerByPuuid', () => {
     let client;
 
     beforeEach(() => {
-        client = {
-            'lol': {
-                "api_limit": false,
-                "route": {
-                    "EUW1": "EUROPE",
-                    "NA1": "AMERICAS",
-                    "KR": "ASIA",
-                    "EUN1": "EUROPE",
-                    "BR1": "AMERICAS",
-                    "JP1": "ASIA",
-                    "LA1": "AMERICAS",
-                    "LA2": "AMERICAS",
-                    "OC1": "SEA",
-                    "TR1": "EUROPE",
-                    "RU": "EUROPE"
-                }
-            }
-        }; // mock client object
+        client = client_preset; // mock client object
     });
 
     it('should return a summoner object when given valid input', async () => {
@@ -146,5 +113,83 @@ describe('summonerByPuuid', () => {
         const error = new Error('API call failed');
         api.apiCall = jest.fn().mockRejectedValue(error);
         await expect(api.summonerByPuuid(api_key, 'EUW1', '4NrXpmBZPs961u7WfD2_BJ922QBAn8eTgtKKIbpcG1W_wbGIeEwTQfdkAzGb6dBp11JNHhCgWcEaRA', client)).rejects.toThrow(error);
+    });
+});
+
+describe('challenge', () => {
+    let client;
+
+    beforeEach(() => {
+        client = client_preset; // mock client object
+    });
+
+    it('should return a challenge object when given valid input', async () => {
+        const api = new LolApi();
+        const challenge = await api.challenge(api_key, 'EUW1', client);
+        expect(challenge).toHaveProperty('percentiles');
+    });
+
+    it('should throw an error when given an invalid region', async () => {
+        const api = new LolApi();
+        await expect(api.challenge(api_key, 'invalid', client)).rejects.toThrow();
+    });
+
+    it('should throw an error when the API call fails', async () => {
+        const api = new LolApi();
+        const error = new Error('API call failed');
+        api.apiCall = jest.fn().mockRejectedValue(error);
+        await expect(api.challenge(api_key, 'EUW1', client)).rejects.toThrow(error);
+    });
+});
+
+describe('challengeconfig', () => {
+    let client;
+
+    beforeEach(() => {
+        client = client_preset; // mock client object
+    });
+
+    it('should return a challenge config object when given valid input', async () => {
+        const api = new LolApi();
+        const challengeConfig = await api.challengeconfig(api_key, 'EUW1', client);
+        expect(challengeConfig).toHaveProperty('gameModeToConfig');
+    });
+
+    it('should throw an error when given an invalid region', async () => {
+        const api = new LolApi();
+        await expect(api.challengeconfig(api_key, 'invalid', client)).rejects.toThrow();
+    });
+
+    it('should throw an error when the API call fails', async () => {
+        const api = new LolApi();
+        const error = new Error('API call failed');
+        api.apiCall = jest.fn().mockRejectedValue(error);
+        await expect(api.challengeconfig(api_key, 'EUW1', client)).rejects.toThrow(error);
+    });
+});
+
+describe('challengebypuuid', () => {
+    let client;
+
+    beforeEach(() => {
+        client = client_preset; // mock client object
+    });
+
+    it('should return a challenge object when given valid input', async () => {
+        const api = new LolApi();
+        const challenge = await api.challengebypuuid(api_key, 'EUW1', '4NrXpmBZPs961u7WfD2_BJ922QBAn8eTgtKKIbpcG1W_wbGIeEwTQfdkAzGb6dBp11JNHhCgWcEaRA', client);
+        expect(challenge).toHaveProperty('percentiles');
+    });
+
+    it('should throw an error when given an invalid region', async () => {
+        const api = new LolApi();
+        await expect(api.challengebypuuid(api_key, 'invalid', '4NrXpmBZPs961u7WfD2_BJ922QBAn8eTgtKKIbpcG1W_wbGIeEwTQfdkAzGb6dBp11JNHhCgWcEaRA', client)).rejects.toThrow();
+    });
+
+    it('should throw an error when the API call fails', async () => {
+        const api = new LolApi();
+        const error = new Error('API call failed');
+        api.apiCall = jest.fn().mockRejectedValue(error);
+        await expect(api.challengebypuuid(api_key, 'EUW1', '4NrXpmBZPs961u7WfD2_BJ922QBAn8eTgtKKIbpcG1W_wbGIeEwTQfdkAzGb6dBp11JNHhCgWcEaRA', client)).rejects.toThrow(error);
     });
 });
