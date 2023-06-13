246bdc75d83cbb5d4559c82d45d9b9093b8c4e47
KwikKill <gabriel@blaisot.org>
2023-06-13 10:20:58 +0200

    feat(lol_api): switch backend from node-fetch to axios

--- util/lol_api.js
+++ util/lol_api.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
 const config = require('../config.json');
 const logger = require('./logger.js');
+const axios = require('axios')
 
 const delay_time = 10000;
 const delay = ms => new Promise(res => setTimeout(res, ms));
@@ -271,57 +271,56 @@ class LolApi {
                 logger.log("API CALL: " + url);
             }
             // Fetch Data from provided URL & Options
-            const responsefetch = await fetch(url);
-            const data = await responsefetch.json();
-            let statut = await responsefetch;
-            statut = statut.status;
-            switch (statut) {
-                case 200:
-                    client.lol.api_limit = false;
-                    return data;
-                case 429:
-                    // Special Handling here - 429 is Rate Limit Reached.
-                    // Alert the User
-                    if (config.verbose) {
-                        logger.error("Limite d'appel de l'API atteinte.  Mise pause du script et reprise dans 10 secondes.", "429");
-                    }
-                    client.lol.api_limit = true;
-                    // Wait the time specified by the reponse header
-                    //await client.
-                    await delay(delay_time);
-                    // Retry
-                    return await this.apiCall(url, client);
-                case 404:
-                    logger.error("La ressource demandée n'existe pas. " + url, "404");
-                    return null;
-                case 400:
-                    logger.error("La requête est invalide." + url, "400");
-                    return null;
-                case 403:
-                    logger.error("La clé API n'est pas valide." + url, "403");
-                    return null;
-                case 503:
-                    logger.error("Le service est temporairement indisponible. Mise en pause. " + url, "503");
-                    await delay(60000);
-                    return await this.apiCall(url, client);
-                default:
-                    logger.error("Erreur inconnue: " + statut + " " + url);
-                    if (data) {
-                        return data;
-                    }
-                    return null;
-            }
+            const responsefetch = await axios.get(url);
+            const data = responsefetch.data;
+            client.lol.api_limit = false;
+            return data;
         } catch (error) {
-            // catch Invalid JSON
-            if (error.type === 'invalid-json') {
-                throw error;
+            if (err.response?.status === 429) {
+                // Special Handling here - 429 is Rate Limit Reached.
+                // Alert the User
+                if (config.verbose) {
+                    logger.error("Limite d'appel de l'API atteinte.  Mise pause du script et reprise dans 10 secondes.", "429");
+                }
+                client.lol.api_limit = true;
+                // Wait the time specified by the reponse header
+                //await client.
+                await delay(delay_time);
+                // Retry
+                return await this.apiCall(url, client);
+            } else if (err.response?.status === 404) {
+                logger.error("La ressource demandée n'existe pas. " + url, "404");
+                return null;
+            } else if (err.response?.status === 400) {
+                logger.error("La requête est invalide." + url, "400");
+                return null;
+            } else if (err.response?.status === 403) {
+                logger.error("La clé API n'est pas valide." + url, "403");
+                return null;
+            } else if (err.response?.status === 503) {
+                logger.error("Le service est temporairement indisponible. Mise en pause. " + url, "503");
+                await delay(60000);
+                return await this.apiCall(url, client);
+            } else if (err.response?.status !== undefined) {
+                logger.error("Erreur inconnue: " + statut + " " + url);
+                if (data) {
+                    return data;
+                }
+                return null;
+            } else {
+                // catch Invalid JSON
+                if (error.type === 'invalid-json') {
+                    throw error;
+                }
+                logger.error(error.name + " : " + url);
+                console.log(error);
+                await delay(1000);
+                return await this.apiCall(url, client);
             }
-            logger.error(error.name + " : " + url);
-            console.log(error);
-            await delay(1000);
-            return await this.apiCall(url, client);
         }
+
     }
+}
 
     /**
     * get champion stats
@@ -332,26 +331,26 @@ class LolApi {
     * @returns {Object}    champion stats
     */
     async championStaticData(apiKey, language, patch, client) {
-        // If language isn't Set - Default to English
-        if (language === "") {
-            language = "en_US";
-        }
-
-        try {
-            const url = "https://ddragon.leagueoflegends.com/cdn/" + patch + "/data/" + language + "/champion.json" + "?api_key=" + apiKey;
-            //Logger.log(url)
-            return this.apiCall(url, client);
-        } catch (error) {
-            logger.error(error);
-        }
-
-        try {
-            const url = "https://ddragon.leagueoflegends.com/cdn/10.9.1/data/" + language + "/champion.json" + "?api_key=" + apiKey;
-            return await this.apiCall(url, client);
-        } catch (error) {
-            logger.error(error);
-        }
+    // If language isn't Set - Default to English
+    if (language === "") {
+        language = "en_US";
     }
+
+    try {
+        const url = "https://ddragon.leagueoflegends.com/cdn/" + patch + "/data/" + language + "/champion.json" + "?api_key=" + apiKey;
+        //Logger.log(url)
+        return this.apiCall(url, client);
+    } catch (error) {
+        logger.error(error);
+    }
+
+    try {
+        const url = "https://ddragon.leagueoflegends.com/cdn/10.9.1/data/" + language + "/champion.json" + "?api_key=" + apiKey;
+        return await this.apiCall(url, client);
+    } catch (error) {
+        logger.error(error);
+    }
+}
 
     /**
     * get champion list
@@ -362,56 +361,56 @@ class LolApi {
     */
     async championList(api_key, region, language, client) {
 
-        // Get New Champion List from Data Dragon
-        const patch = await this.getCurrentPatch(region, client);
-        const championList = await this.championStaticData(api_key, language, patch['v'], client);
+    // Get New Champion List from Data Dragon
+    const patch = await this.getCurrentPatch(region, client);
+    const championList = await this.championStaticData(api_key, language, patch['v'], client);
 
-        //Logger.log(championList['data'])
+    //Logger.log(championList['data'])
 
-        // Champions Array
-        const champions = [];
+    // Champions Array
+    const champions = [];
 
-        // Add Champion Names & IDs to the array
-        for (const champion in championList['data']) {
-            champions[championList['data'][champion]['key']] = championList['data'][champion]['name'];
-        }
-
-        return champions;
+    // Add Champion Names & IDs to the array
+    for (const champion in championList['data']) {
+        champions[championList['data'][champion]['key']] = championList['data'][champion]['name'];
     }
 
-    /**
-    * Check region validity
-    * @function checkRegion
-    * @param {*} region    region of the server
-    * @param {*} client    discord client
-    * @returns {Boolean}   true if region is valid
-    * @throws {Error}      if region is invalid
-    */
-    checkRegion(region, client) {
-        // Check if region is valid
-        if (client.lol.route[region] !== undefined) {
-            return true;
-        }
-        throw new Error("Invalid Region");
+    return champions;
+}
 
+/**
+* Check region validity
+* @function checkRegion
+* @param {*} region    region of the server
+* @param {*} client    discord client
+* @returns {Boolean}   true if region is valid
+* @throws {Error}      if region is invalid
+*/
+checkRegion(region, client) {
+    // Check if region is valid
+    if (client.lol.route[region] !== undefined) {
+        return true;
     }
+    throw new Error("Invalid Region");
 
-    /**
-    * Check route validity
-    * @function checkRoute
-    * @param {*} route    route of the server
-    * @param {*} client    discord client
-    * @returns {Boolean}   true if route is valid
-    * @throws {Error}      if route is invalid
-    */
-    checkRoute(route, client) {
-        // Check if region is valid
-        if (client.lol.routes.includes(route)) {
-            return true;
-        }
-        throw new Error("Invalid Route");
+}
 
+/**
+* Check route validity
+* @function checkRoute
+* @param {*} route    route of the server
+* @param {*} client    discord client
+* @returns {Boolean}   true if route is valid
+* @throws {Error}      if route is invalid
+*/
+checkRoute(route, client) {
+    // Check if region is valid
+    if (client.lol.routes.includes(route)) {
+        return true;
     }
+    throw new Error("Invalid Route");
+
+}
 }
 
 module.exports = {
